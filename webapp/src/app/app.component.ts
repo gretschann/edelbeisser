b097c1f6adbcc8996731190709d459d114d541c1
gretschann <ag@netwunder.de>
2017-12-20 10:12:48 +0100

    added apollo

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
+
+const submitRepository = gql`
+mutation submitRepository {
+  submitRepository(repoFullName: "apollographql/apollo-client") {
+    createdAt
+  }
+}
+`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  constructor( apollo: Apollo, httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({ uri: 'https://api.graph.cool/simple/v1/cj1dk7m2nmfl00113c368h1q0'}),
+      cache: new InMemoryCache()
+    });
+
+    // make a request
+    apollo.query({query: gql`{ allProducts {id, createdAt, name, description} }`}).subscribe(console.log);
+  }
+  getProducts(){
+    console.log('get');
+    
+  }
   title = 'app';
   messages = ['One', 'Second'];
 }
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,7 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './routes/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/* Apollo */
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule } from 'apollo-angular';
+import { HttpLinkModule } from 'apollo-angular-link-http';
+
+/* Material */
 import { MatButtonModule, MatCheckboxModule, MatListModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
@@ -13,9 +22,13 @@ import { MatButtonModule, MatCheckboxModule, MatListModule } from '@angular/mate
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule,
     MatButtonModule,
     MatCheckboxModule,
-    MatListModule
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
